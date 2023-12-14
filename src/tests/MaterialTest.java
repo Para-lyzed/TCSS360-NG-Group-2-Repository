61fa9b8718623598e55a002cddf8e4b198ff7d4a
Cody <itsmobnt@gmail.com>
2023-12-13 22:36:18 -0800

    Wrote tests classes for About.java, Material.Java, Profile.java, and Tool.java.

--- src/tests/MaterialTest.java
+++ src/tests/MaterialTest.java
@@ -1,4 +1,48 @@
 package tests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import model.Material;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * TCSS 360B
+ * Team MVP - Deliverable 3
+ * tests.MaterialTest.java
+ *
+ * @author Cody Dukes
+ *
+ */
 public class MaterialTest {
+    private Material myDefaultMaterial;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        myDefaultMaterial = new Material("Super Glue", "Adhesives", 25);
+    }
+
+    @Test
+    void testMaterialName() {
+        final String expectedName = "Super Glue";
+
+        assertEquals(expectedName, myDefaultMaterial.getName(),
+                "This assert tests the default Material name");
+    }
+
+    @Test
+    void testMaterialPrice() {
+        final int expectedPrice = 25;
+
+        assertEquals(expectedPrice, myDefaultMaterial.getPrice(),
+                "This assert tests the default Material price");
+    }
+
+    @Test
+    void testMaterialCategory() {
+        final String expectedCategory = "Adhesives";
+
+        assertEquals(expectedCategory, myDefaultMaterial.getCategory(),
+                "This assert tests the default Material category");
+    }
 }
--- src/tests/ToolTest.java
+++ src/tests/ToolTest.java
@@ -1,17 +1,40 @@
 package tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import model.Tool;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+/**
+ * TCSS 360B
+ * Team MVP - Deliverable 3
+ * tests.ToolTest.java
+ *
+ * @author Cody Dukes
+ *
+ */
 public class ToolTest {
     private Tool myDefaultTool;
 
     @BeforeEach
     void setUp() throws Exception {
         myDefaultTool = new Tool("Toy-Hammer", 15);
+    }
+
+    @Test
+    void testToolName() {
+        final String expectedName = "Toy-Hammer";
+
+        assertEquals(expectedName, myDefaultTool.getName(),
+                "This assert tests the default Tool name");
+    }
+
+    @Test
+    void testToolPrice() {
+        final int expectedPrice = 15;
+
+        assertEquals(expectedPrice, myDefaultTool.getPrice(),
+                "This assert tests the default Tool price");
     }
 }
