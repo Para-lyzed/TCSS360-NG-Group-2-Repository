4f91acce30d4149a8357876e276a669209bd0519
Para-lyzed <para_lyz3d@protonmail.com>
2023-12-16 02:13:33 -0800

    Removed unnecessary dependencies

--- src/view/ProjectExpensePanel.java
+++ src/view/ProjectExpensePanel.java
@@ -2,10 +2,8 @@ package view;
 
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
-import java.awt.Robot;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 import java.util.HashMap;
 import java.util.LinkedList;
 
